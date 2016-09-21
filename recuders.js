d7b149d7582cc93b4caf09bbe036086d88b90743
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-20 21:30:00 -0700

    Writing all the reducers for the state of the application.

---
+++ recuders.js
@@ -0,0 +1,257 @@
+const initialUserState = {
+  lastUpdated: 0,
+  info: {
+    name: '',
+    uid: '',
+    avatar: ''
+  }
+}
+
+function user (state = initialUserState, action) {
+  switch (action.type) {
+    case FETCHING_USER_SUCCESS:
+      return {
+        ...state,
+        info: action.user,
+        lastUPdated: action.timestamp
+      }
+      default:
+        return state
+  }
+}
+
+const initialState = {
+  isFetching: false,
+  error: '',
+  isAuthed: false,
+  authedId: ''
+}
+
+export default function users (state = initialState, action) {
+  switch (action.type) {
+    case AUTH_USER:
+      return {
+        ...state,
+        isAuthed: true,
+        authedId: action.uid
+      }
+    case UNAUTH_USER:
+      return {
+        ...state,
+        isAuthed: false,
+        authedId: ''
+      }
+    case FETCHING_USER:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case FETCHING_USER_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case FETCHING_USER_SUCCESS:
+      return action.user === null
+        ? {
+          ...state,
+          isFetching: false,
+          error: '',
+        }
+        : {
+          ...state,
+          isFetching: false,
+          error: '',
+          [action.uid]: user(state[action.uid], action)
+        }
+    default:
+      return state
+  }
+}
+
+// ducks
+
+const initialState = {
+  isFetching: true,
+  error: ''
+}
+
+export default function ducks (state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_DUCK:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case ADD_DUCK:
+    case FETCHING_DUCK_SUCCESS:
+      return {
+        ...state,
+        error: '',
+        isFetching: false,
+        [action.duck.duckId]: action.duck
+      }
+    case FETCHING_DUCK_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case REMOVE_FETCHING:
+      return {
+        ...state,
+        error: '',
+        isFetching: false
+      }
+    case ADD_MULTIPLE_DUCKS:
+      return {
+        ...state,
+        ...action.ducks
+      }
+    default:
+      return state
+  }
+}
+
+// feed
+
+const initialState = {
+  newDucksAvailable: false,
+  newDucksToAdd: [],
+  isFetching: false,
+  error: '',
+  duckIds: []
+}
+
+export default function feed (state = initialState.action) {
+  switch (action.type) {
+    case SETTING_FEED_LISTENER:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case SETTING_FEED_LISTENER_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case SETTING_FEED_LISTENER_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+        duckIds: action.duckIds,
+        newDucksAvailable: false
+      }
+    case ADD_NEW_DUCK_ID_TO_FEED:
+      return {
+        ...state,
+        newDucksToAdd: [actioin.duckId, ...state.newDucksToAdd],
+        newDucksAvailable: true
+      }
+    case RESET_NEW_DUCKS_AVAILABLE:
+      return {
+        ...state,
+        duckIds: [...state.newDucksToAdd, ...state.duckIds]
+      }
+    default:
+      return state
+  }
+}
+
+// listeners
+
+export default function listeners (state = {}, action) {
+  switch (action.type) {
+    case ADD_LISTENER:
+      return {
+        ...state,
+        [action.listenerId]: true
+      }
+    default:
+      return state
+  }
+}
+
+//modal
+
+const initialState = {
+  duckText: '',
+  isOpen: false
+}
+
+export default function modal (state = initialState, action) {
+  switch (action.type) {
+    case OPEN_MODAL:
+      return {
+        ...state,
+        isOpen: true
+      }
+    case CLOSE_MODAL:
+      return {
+        duckText: '',
+        isOpen: false
+      }
+    case UPDATE_DUCK_TEXT:
+      return {
+        ...state,
+        duckText: aciton.newDuckText
+      }
+    default:
+      return state
+  }
+}
+
+// replies
+
+const initialReply = {
+  name: '',
+  reply: '',
+  uid: '',
+  timestamp: 0,
+  avatar: '',
+  replyId: ''
+}
+
+function duckReplies (state = initialReply, action) {
+  switch (action.type) {
+    case ADD_REPLY:
+      return {
+        ...state,
+        [action.reply.replyId]: action.reply
+      }
+    case REMOVE_REPLY:
+      return {
+        ...state,
+        [action.reply.replyId]: undefined
+      }
+    default:
+      return state
+  }
+}
+
+const initialDuckState = {
+  lastUpdated: Date.now(),
+  replies: {}
+}
+
+function repliesAndLastUpdated (state = initialDuckState, action) {
+  switch (action.type) {
+    case FETCHING_REPLIES_SUCCESS:
+      return {
+        ...state,
+        lastUpdated: action.lastUpdated,
+        replies: action.replies
+      }
+    case ADD_REPLY:
+    case REMOVE_REPLY:
+      return {
+        ...state,
+        replies: duckReplies(state.replies, action)
+      }
+    default:
+      return state
+  }
+}
