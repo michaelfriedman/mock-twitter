f31153f6f29a5af9188747ed615ea293b079e910
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-20 23:09:53 -0700

    react router setup and ready to go.

---
+++ app/Containers/Main/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainContainer = React.createClass({
+  render () {
+    return (
+      <p>Hello my name is Michael!</p>
+    )
+  }
+})
+
+export default MainContainer
