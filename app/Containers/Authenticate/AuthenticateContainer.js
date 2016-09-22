64134e032d4b09dca7f351b01df857371ec508ba
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-22 13:55:31 -0700

    redux-thunks

--- app/Containers/Authenticate/AuthenticateContainer.js
+++ app/Containers/Authenticate/AuthenticateContainer.js
@@ -7,20 +7,12 @@ import * as userActionCreators from 'redux/modules/users'
 
 const AuthenticateContainer = React.createClass({
   propTypes: {
-    fetchingUser: PropTypes.func.isRequired,
-    fetchingUserFailure: PropTypes.func.isRequired,
-    fetchingUserSuccess: PropTypes.func.isRequired,
     isFetching: PropTypes.bool.isRequired,
-    error: PropTypes.string.isRequired
+    error: PropTypes.string.isRequired,
+    fetchAndHandleAuthedUser: PropTypes.func.isRequired
   },
   handleAuth () {
-    this.props.fetchingUser()
-    auth().then((user) => {
-      console.log('user', user)
-      this.props.fetchingUserSuccess(user.uid, user, Date.now())
-      this.props.authUser(user.uid)
-    })
-    .catch((error) => this.props.fetchingUserFailure(error))
+    this.props.fetchAndHandleAuthedUser()
   },
   render () {
     return (
